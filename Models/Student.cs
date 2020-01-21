df1336f747511476c3446a0ce5eb1cfce7c6ca39
Mehdi Rifi <rifi.mehdi@regsquare.com>
2020-01-21 10:43:12 +0100

    initial project install packages and implement the first IOC and DI for the first class Student

---
+++ Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tp3_MVC.Models
+{
+    public partial class Student
+    {
+        [Key]
+        [Column("codeStudent", TypeName="int")]
+        [Required]
+        public int codeStudent { get; set; }
+        [Column("codeStudent", TypeName = "VARCHAR(30)")]
+        [Required(ErrorMessage ="This field is required")]
+        [DisplayName("Full Name")]
+        public string fullName { get; set; }
+        [Column("old", TypeName = "int")]
+        [Required(ErrorMessage = "This field is required")]
+        [DisplayName("Student's old")]
+        public int old { get; set; }
+    }
+}
---
+++ ModelsContext/GroupDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tp3_MVC.Models;
+
+namespace Tp3_MVC.ModelsContext
+{
+    public class GroupDbContext : DbContext
+    {
+        public GroupDbContext(DbContextOptions<GroupDbContext> options) : base(options)
+        {
+
+        }
+        public virtual DbSet<Student> Students { get; set; }
+    }
+}
