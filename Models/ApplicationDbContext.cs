5a03491f4add55bde1bfcc8f6cb33a43ea7629d4
EL BAHLOUL Abdelali <bahloul97abdel@gmail.com>
2020-01-23 12:16:41 +0100

    add aspnetCOre.identity

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tp3_MVC.Models
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Student>()
+                .HasIndex(key => key.codeStudent)
+                .IsUnique();
+        }
+
+
+    }
+
+
+
+}
--- Startup.cs
+++ Startup.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Tp3_MVC.Models;
 using Tp3_MVC.ModelsContext;
 
 namespace Tp3_MVC
@@ -29,6 +31,8 @@ namespace Tp3_MVC
             services.AddDbContext<GroupDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DevConnection"))
             );
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
